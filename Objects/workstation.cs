ba5dfb9eab0929748432af80025db76c7daeff9f
BrodyBrownlee <brodybrownlee@mmc.school.nz>
2024-01-25 14:36:05 +1300

    Added workstation upgrades
    Need to add resource requirements, and variables change based on upgrades

--- Objects/workstation.cs
+++ Objects/workstation.cs
@@ -3,27 +3,46 @@ using System;
 
 public partial class workstation : Node2D
 {
-	int TorchLV = 1, LampLV = 1;
+	int LanternLV = 1, LampLV = 1, ResourceLV = 1, CampfireLV = 1;
+	bool campfireUnlock = false, LampUnlock = false;
 	bool PlayerInRange = false;
 	bool PlayerInteracting = false;
 	Area2D ForcedInteractionRange;
 	Control GUIContainer;
-	Label workstationLbl, torchLevelLbl, lampLevelLbl;
+	Label workstationLbl, lanternLevelLbl, lampLevelLbl, campfireLevelLbl, resourceLevelLbl;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ForcedInteractionRange = GetNode<Area2D>("ForcedInteractionRange");
 		GUIContainer = GetNode<Control>("displayGUIControl");
 		workstationLbl = GetNode<Label>("workstationLabel");
-		torchLevelLbl = GetNode<Label>("displayGUIControl/lblTorchLevel");
+		lanternLevelLbl = GetNode<Label>("displayGUIControl/lblLanternLevel");
 		lampLevelLbl = GetNode<Label>("displayGUIControl/lblLampLevel");
+		campfireLevelLbl = GetNode<Label>("displayGUIControl/lblCampfireUnlock");
+		resourceLevelLbl = GetNode<Label>("displayGUIControl/lblResourcelvl");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		torchLevelLbl.Text = "Torch Upgrade Level: " + TorchLV;
-		lampLevelLbl.Text = "Lamp Upgrade Level: " + LampLV;
+		lanternLevelLbl.Text = "Lantern Upgrade Level: " + LanternLV;
+		resourceLevelLbl.Text = "Resource Collection Level: " + ResourceLV;
+		if (LampUnlock)
+        {
+			lampLevelLbl.Text = "Lamp Upgrade Level: " + LampLV;
+        }
+        else
+        {
+			lampLevelLbl.Text = "Unlock Lamp Post: ";
+        }
+		if(campfireUnlock)
+        {
+			campfireLevelLbl.Text = "Campfire Unlocked";
+        }
+        else
+        {
+			campfireLevelLbl.Text = "Unlock Campfire: ";
+        }
 	}
 
 	private void inInteractionArea(Node2D body)
@@ -40,25 +59,62 @@ public partial class workstation : Node2D
 	private void changeVisibility(bool PlayerInRange, bool PlayerInteracting)
     {
 			GUIContainer.Visible = PlayerInteracting;
+        if (!PlayerInteracting)
+        {
 			workstationLbl.Visible = PlayerInRange;
+		}
+        else
+        {
+			workstationLbl.Visible = false;
+        }
 	}
-	private void torchUpgradeCheck()
+	private void lanternUpgradeCheck()
 	{
-		doTorchUpgrade();
+		doLanternUpgrade();
 	}
 	private void lampUpgradeCheck()
     {
+		if(!LampUnlock)
+        {
+			LampUnlock = true;
+        }
+        else
+        {
+			doLampUpgrade();
+		}
 		
-		doLampUpgrade();
     }
-	private void doTorchUpgrade()
+	private void campfireUpgradeCheck()
     {
-		TorchLV++;
+		if(!campfireUnlock)
+        {
+			campfireUnlock = true;
+        }
+        else
+        {
+			doCampfireUpgrade();
+        }
+    }
+	private void resourceUpgradeCheck()
+    {
+		doResourceUpgrade();
+    }
+	private void doResourceUpgrade()
+    {
+		ResourceLV++;
+    }
+	private void doLanternUpgrade()
+    {
+		LanternLV++;
 	}
 	private void doLampUpgrade()
     {
 		LampLV++;
 	}
+	private void doCampfireUpgrade()
+    {
+
+    }
     public override void _Input(InputEvent @event)
     {
 		if (@event.IsActionPressed("Interact") && PlayerInRange == true)
